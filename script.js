cd23acf1ab49b4b283d99e081b6129f4af5854e6
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-28 14:23:02 +0200

    "Refactored script.js: reformatted code, added event listeners to buttons"

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-let [computer_score,user_score]=[0,0];
+let [computer_score, user_score] = [0, 0];
 let result_ref = document.getElementById("result");
 let choices_object = {
-    'rock' : {
-        'rock' : 'draw',
-        'scissor' : 'win',
-        'paper' : 'lose'
+    'rock': {
+        'rock': 'draw',
+        'scissor': 'win',
+        'paper': 'lose'
     },
-    'scissor' : {
-        'rock' : 'lose',
-        'scissor' : 'draw',
-        'paper' : 'win'
+    'scissor': {
+        'rock': 'lose',
+        'scissor': 'draw',
+        'paper': 'win'
     },
-    'paper' : {
-        'rock' : 'win',
-        'scissor' : 'lose',
-        'paper' : 'draw'
+    'paper': {
+        'rock': 'win',
+        'scissor': 'lose',
+        'paper': 'draw'
     }
-
 }
 
-function checker(input){
+function checker(input) {
     var choices = ["rock", "paper", "scissor"];
-    var num = Math.floor(Math.random()*3);
+    var num = Math.floor(Math.random() * 3);
 
     document.getElementById("comp_choice").innerHTML =
-    ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
+        ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
 
     document.getElementById("user_choice").innerHTML =
-    ` You choose <span> ${input.toUpperCase()} </span>`;
+        ` You choose <span> ${input.toUpperCase()} </span>`;
 
     let computer_choice = choices[num];
 
-    switch(choices_object[input][computer_choice]){
+    switch (choices_object[input][computer_choice]) {
         case 'win':
             result_ref.style.cssText = "background-color: #cefdce; color: #689f38";
             result_ref.innerHTML = "YOU WIN";
@@ -51,3 +50,11 @@ function checker(input){
     document.getElementById("computer_score").innerHTML = computer_score;
     document.getElementById("user_score").innerHTML = user_score;
 }
+
+// Add event listeners to buttons
+document.querySelectorAll(".weapons button").forEach(button => {
+    button.addEventListener("click", () => {
+        let user_choice = button.querySelector("i").classList[1].split('-')[2];
+        checker(user_choice);
+    });
+});
